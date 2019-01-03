ec17e3457a7d0718594c951cbfb46dc3b8aa9b3b
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:38:08 +0700

    Add files via upload

---
+++ program perkalian dua pecahan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float hasil;
+    cout<<"Program perkalian 2 pecahan"<<endl;
+    cout<<"P1= 3.0 / 4.0"<<endl;
+    cout<<"P2= 2.0 / 3.0"<<endl;
+    hasil=(3.0*2.0) / (3.0*4.0);
+    cout<<"Hasilnya adalah : " <<hasil;
+    return 0;
+}
